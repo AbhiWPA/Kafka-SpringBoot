c0cabe611bfa6e303cc6f6e46ef92a8d6368d043
AbhiWPA <abhiashinsa770@gmail.com>
2023-07-18 02:45:21 -0700

    ISO8583 message with kafka

---
+++ Kafka-ISO8583-Message/src/main/java/lk/epic/iso8583kafka/Iso8583kafkaApplication.java
@@ -0,0 +1,13 @@
+package lk.epic.iso8583kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iso8583kafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iso8583kafkaApplication.class, args);
+    }
+
+}
---
+++ Kafka-ISO8583-Message/src/test/java/lk/epic/iso8583kafka/Iso8583kafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.epic.iso8583kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iso8583kafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
